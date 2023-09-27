ea098a8b62240db919d9b55e4c4c967e4aa47a15
Rafael-Rodrigues-64 <rafarodrigs3@gmail.com>
2023-09-27 19:42:22 -0300

    Initial commit

---
+++ Estrutura Sequencial/Exemplo.py
@@ -0,0 +1,44 @@
+# Comentar vária slinhas: CTRL + k + c
+# Limpar Terminal: CTRL + L
+
+# idade: int
+# salario: float; altura: float
+# genero: str; nome: str
+
+# print("Bom dia", end="")
+# print("Boa Noite", end="")
+
+# x: float
+# x = 2.3456
+# print("{:.2f}".format(x))
+# print(f"{x:.2f}")
+
+# idade = 20
+# salario = 5800.5
+# altura = 1.63
+# genero = "F"
+# nome = "Maria Silva"
+
+# print(f"IDADE = {idade}")
+# print(f"SALÁRIO = {salario:.2f}")
+# print(f"ALTURA = {altura:.2f}")
+# print(f"GÊENERO = {genero}")
+# print(f"NOME = {nome}")
+
+# print(f"A funcionária {nome}, sexo {genero}, altura {altura}, salario R$ {salario:.2f} reais e tem idade {idade} anos")
+
+# b1: float; b2: float; h: float; area: float; 
+# b1 = 6.0
+# b2 = 8.0
+# h = 5.0
+# area = (b1 + b2) / 2.0 * h
+# print(f"O valor da área é: {area}")
+
+a: int; b: int; resultado: int
+a = 7
+b = 2
+resultado = a % b
+resultado0 = a / b
+print(f"O resto da divisão de a com b é: {resultado}")
+print(f"O resultado da divisão de a com b é: {resultado0}")
+print(f"A divisão inteira entre a e b é: {resultado0}")
---
+++ Estrutura Sequencial/Transformar.py
@@ -0,0 +1,21 @@
+nome  = str; nome0  = str
+salario = float; salario0 = float
+
+nome = input("Nome da Primeira Pessoa: ")
+salario = float(input("Salário da Primeira Pessoa: "))
+
+nome0 = input("Nome da Segunda Pessoa: ")
+salario0 = float(input("Salário da Segunda Pessoa: "))
+
+idade = int(input("Digite uma idade"))
+sexo = input("Digite um sexo (F/M): ")
+
+print("-------------------------------------")
+print("Nome da primeira Pessoa: ",nome)
+print(f"Salario da primeira Pessoa: R$ Silvia{salario}")
+print("-------------------------------------")
+print("Nome da segunda Pessoa: ",nome0)
+print(f"Salario da segunda Pessoa: R$ {salario0:.2f}")
+print("-------------------------------------")
+print(f"Idade {idade}")
+print(f"Sexo {sexo}")

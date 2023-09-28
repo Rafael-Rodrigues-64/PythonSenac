ca24ab345112481642f8be7248853ddb258745af
Rafael-Rodrigues-64 <rafarodrigs3@gmail.com>
2023-09-28 19:34:08 -0300

    Create desafio3.py
    Terceiro Exercício

---
+++ Estrutura Sequencial/desafio3.py
@@ -0,0 +1,18 @@
+preco = 2100.5
+preco1 = 1830.0
+
+produto = "computador"
+produto1 = "TV"
+
+codigo = 5291
+
+idade = 30
+
+genero = "F"
+
+print("Produtos: ")
+print(f"O produto {produto} custa R$ {preco:.2f}")
+print(f"O produto {produto1} custa R$ {preco1:.2f}")
+print()
+print(f"Código do produto = {codigo}")
+print(f"Dados das pessoa: gênero {genero} e idade {idade}")

55bdede517a5ecf096aee1abd2d76a0ef488f97d
Rafael-Rodrigues-64 <rafarodrigs3@gmail.com>
2023-10-10 21:38:17 -0300

    Repetição
    Est. de Repetição

---
+++ Estruturas_Repetitivas/desafio2.py
@@ -0,0 +1,14 @@
+soma = 0
+contador = 0
+idades = int(input("Digite 1ª idade: "))
+while (idades > 0 ):
+    soma += idades
+    contador += 1
+    idades = int(input("Digite a "  ))
+
+    if idades < 0:
+        print("IMPOSSÍVEL CALCULAR com o número: ",idades )    
+        break
+else:
+    media = soma/contador
+    print("A média das Idades é ",idades)  
--- PythonSenacExercicios/PythonSenacExercicios/Condicionais/desafio_Jogo_8_11.py
+++ PythonSenacExercicios/PythonSenacExercicios/Condicionais/desafio_Jogo_8_11.py
@@ -5,14 +5,14 @@ horaFinal = int(input('Digite a hora final do Jogo: '))
 if ((horaFinal >=1 ) and (horaFinal <= 24)) and ((horaInicial >= horaFinal)):
     hora = (24 - horaInicial) + horaFinal
     print(f'A duração do jogo foi de: {hora} horas')
-    print(f'Hora Início: {horaInicial}, Hora Fim: {horaFinal}')
+    print(f'Hora Início: {horaInicial} horas, Hora Fim: {horaFinal} horas6')
 
 
 elif ((horaInicial >= 1) and (horaFinal < 23)) and ((horaInicial < horaFinal)):
     hora = horaFinal - horaInicial
     print(f'A duração do jogo foi de: {hora} horas')
-    print(f'Hora Início: {horaInicial}, Hora Fim: {horaFinal}')
+    print(f'Hora Início: {horaInicial} horas, Hora Fim: {horaFinal} horas')
 
 else:
     print('Horarário(s) incorretos para o jogo!')
-    print(f'Hora Início: {horaInicial}, Hora Fim: {horaFinal}')
+    print(f'Hora Início: {horaInicial} horas, Hora Fim: {horaFinal} horas')
